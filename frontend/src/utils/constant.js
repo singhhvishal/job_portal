d783f464f5e7b056edf5d0b3fd5bc7169e344bde
singhhvishal <115359836+singhhvishal@users.noreply.github.com>
2024-08-13 16:28:52 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,10 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// const backendUrl = https://job-portal-backend-7k7t.onrender.com;
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+
+export const USER_API_END_POINT="https://job-portal-backend-7k7t.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-backend-7k7t.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-backend-7k7t.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-backend-7k7t.onrender.com/api/v1/company";
