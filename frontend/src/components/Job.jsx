2e1d5f09db4e436aecc67234911ba0434b2d5d1a
singhhvishal <vishalsinghnagvanshi0000@gmail.com>
2024-08-15 17:56:45 +0530

    job card improved

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -18,7 +18,7 @@ const Job = ({job}) => {
     
     return (
         <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100'>
-            <div className='flex items-center justify-between'>
+            <div className='flex flex-wrapitems-center justify-between'>
                 <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
                 {/* <Button variant="outline" className="rounded-full" size="icon"><Bookmark /></Button> */}
             </div>
--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { Badge } from './ui/badge'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
-const LatestJobCards = ({job}) => {
+const LatestJobCards = ({ job }) => {
     const navigate = useNavigate();
     return (
-        <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
+        <div
+            onClick={() => navigate(`/description/${job._id}`)}
+            className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer"
+        >
             <div>
-                <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                <p className='text-sm text-gray-500'>India</p>
+                <Button className="p-6" variant="outline" size="icon">
+                    <Avatar>
+                        <AvatarImage src={job?.company?.logo} />
+                    </Avatar>
+                </Button>
+                <h1 className="font-medium text-lg">{job?.company?.name}</h1>
+                <p className="text-sm text-gray-500">India</p>
             </div>
             <div>
-                <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
-                <p className='text-sm text-gray-600'>{job?.description}</p>
+                <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+                <p className="text-sm text-gray-600">{job?.description}</p>
             </div>
-            <div className='flex items-center gap-2 mt-4'>
-                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
-                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
-                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
+            <div className="flex items-center gap-2 mt-4">
+                <Badge className={"text-blue-700 font-bold"} variant="ghost">
+                    {job?.position} Positions
+                </Badge>
+                <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+                    {job?.jobType}
+                </Badge>
+                <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
+                    {job?.salary}LPA
+                </Badge>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default LatestJobCards
+export default LatestJobCards;
